9823a7fc78e598cbf9c44506a322bfd31b70c16b
Vy Nguyen <vyn@metropolia.fi>
2020-01-19 11:23:04 +0200

    fetching data with AJAX task A

--- components/List.js
+++ components/List.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {
   FlatList, View, StyleSheet,
 } from 'react-native';
 import ListItem from './ListItem';
 import PropTypes from 'prop-types';
+import {MediaContext} from '../contexts/MediaContext';
+import {useFetch} from '../hooks/APIHooks';
 
 const List = (props) => {
-  console.log(props);
+  const [media, setMedia] = useContext(MediaContext);
+  const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+  const [data, loading] = useFetch(url);
+  console.log(data, loading);
+  setMedia(data);
   return (
     <View style={styles.container}>
       <FlatList
-        data={props.mediaArray}
+        data={media}
+        keyExtractor={(item, index)=> index.toString()}
         renderItem={({item}) => <ListItem singleMedia={item} />}
       />
     </View>
---
+++ hooks/APIHooks.js
@@ -0,0 +1,18 @@
+// hooks.js
+import {useState, useEffect} from 'react';
+
+const useFetch= (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchUrl= async () =>{
+    const response = await fetch(url);
+    const json = await response.json();
+    setData(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [data, loading];
+};
+export {useFetch};
